7de77cffd9d0a259ce3101e37522519eac826664
ej-kimm <115481749+ej-kimm@users.noreply.github.com>
2024-12-16 23:33:34 +0900

    💄 style : 챔피언 목록 스타일

--- src/components/champions/ChampionCard.tsx
+++ src/components/champions/ChampionCard.tsx
@@ -1,22 +1,29 @@
 import { Champion } from '@/types/Champion'
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 type ChampionCardProps = { champion: Champion }
 
 export default function ChampionCard({ champion }: ChampionCardProps) {
   return (
-    <li>
-      <div className="flex">
-        <p>{champion.name}</p>
-        <p>{champion.title}</p>
-      </div>
-      <Image
-        src={champion.imageUrl}
-        width={120}
-        height={120}
-        alt={champion.id}
-      />
+    <li className="group">
+      <Link href={`/champions/${champion.id}`}>
+        <div className="relative flex flex-col items-center">
+          <div className="w-32 h-32 rounded-full overflow-hidden border-2 border-gray-400">
+            <Image
+              src={champion.imageUrl}
+              width={120}
+              height={120}
+              alt={champion.id}
+            />
+          </div>
+          <div className="flex flex-col mt-2 text-center">
+            <p className="font-semibold">{champion.name}</p>
+            <p className="text-xs text-gray-500">{champion.title}</p>
+          </div>
+        </div>
+      </Link>
     </li>
   )
 }
--- src/components/champions/ChampionList.tsx
+++ src/components/champions/ChampionList.tsx
@@ -6,7 +6,7 @@ type ChampionListProps = { champions: Champion[] }
 
 export default function ChampionList({ champions }: ChampionListProps) {
   return (
-    <ul>
+    <ul className="p-10 gap-5 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-8 justify-items-center place-items-center">
       {champions.map((champion) => (
         <ChampionCard key={champion.key} champion={champion} />
       ))}
