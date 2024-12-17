59e9b5f3e904d85eb947e3b8d79af5a317437144
ej-kimm <115481749+ej-kimm@users.noreply.github.com>
2024-12-18 00:45:13 +0900

    💄 style : 홈 페이지 스타일링

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
+import HomeListCard from '@/components/ui/HomeListCard'
+
 export default function Home() {
-  return <>Home!!</>
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-6xl font-extrabold text-center text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 to-red-500 mb-8 animate__animated animate__fadeIn">
+        리그 오브 레전드 정보 앱
+      </h1>
+      <p className="text-xl text-center mb-12">
+        Riot Games API를 활용하여 챔피언과 아이템 정보를 제공합니다.
+      </p>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+        <HomeListCard src="championsList" text="챔피언 목록 보기" />
+        <HomeListCard src="rotation" text="금주 로테이션 확인" />
+        <HomeListCard src="items" text="아이템 목록 보기" />
+      </div>
+    </div>
+  )
 }
---
+++ src/components/ui/HomeListCard.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+type HomeListProps = {
+  src: string
+  text: string
+}
+
+export default function HomeListCard({ src, text }: HomeListProps) {
+  return (
+    <div className="bg-gray-800 rounded-xl shadow-lg overflow-hidden transform transition-all duration-300 hover:scale-105 hover:shadow-2xl">
+      <Link href="/champions">
+        <Image
+          src={`/assets/${src}.webp`}
+          width={400}
+          height={300}
+          alt={`${src}`}
+          className="w-full h-60 object-cover"
+        />
+        <div className="p-6 text-center">
+          <p className="text-lg font-semibold">{text}</p>
+        </div>
+      </Link>
+    </div>
+  )
+}
