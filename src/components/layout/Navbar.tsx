1a43051c6fc8d5a368d1b4fa4d1bf5e68441d9e1
ej-kimm <115481749+ej-kimm@users.noreply.github.com>
2024-12-18 10:42:42 +0900

    ✨ feat : Navbar에 현재 경로에 따라 label보여주기

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,23 +1,60 @@
 'use client'
 
-import React, { useState } from 'react'
+import React, { ReactNode, useEffect, useState } from 'react'
 import { MdMenu } from 'react-icons/md'
 import Image from 'next/image'
 import Sidebar from './Sidebar'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { LuSwords } from 'react-icons/lu'
+import { GiHelmet } from 'react-icons/gi'
+
+type NavbarContent = {
+  icon: ReactNode
+  label: string
+}
+
+const NAVBAR_CONTENTS: Readonly<Record<string, NavbarContent>> = {
+  champions: {
+    icon: (
+      <Image src="/assets/champion.svg" alt="champion" width={29} height={29} />
+    ),
+    label: '챔피언',
+  },
+  items: {
+    icon: <LuSwords className="text-2xl text-[#CCCCCC]" />,
+    label: '아이템',
+  },
+  rotation: {
+    icon: <GiHelmet className="text-2xl text-[#CCCCCC]" />,
+    label: '로테이션',
+  },
+}
 
 export default function Navbar() {
+  const pathname = usePathname()
+  const [currentPath, setCurrentPath] = useState<string>(pathname)
   const [menuIsOpen, setMenuIsOpen] = useState<boolean>(false)
+  const currentContent = NAVBAR_CONTENTS[currentPath] || {}
 
   const toggleMenu = () => {
     setMenuIsOpen((prev) => !prev)
   }
+
+  useEffect(() => {
+    const path = pathname.split('/')[1]
+    setCurrentPath(path)
+  }, [pathname])
 
   return (
     <header className="fixed top-0 left-0 w-full h-[90px] bg-zinc-900 z-40 px-10 py-4 flex justify-between items-center">
       <Link href="/">
         <Image src="/assets/Logo.png" width={150} height={100} alt="logo" />
       </Link>
+      <div className="flex gap-2 justify-center items-center">
+        {currentContent.icon}
+        <span className="text-2xl font-bold">{currentContent.label}</span>
+      </div>
       <button type="button" onClick={toggleMenu}>
         <MdMenu className="text-4xl" />
       </button>
